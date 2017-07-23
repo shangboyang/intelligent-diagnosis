f69e7d07463625f2637b8f0234c1ca785b2dc323
shangboyang <yang8701@gmail.com>
2017-07-23 22:48:17 +0800

    update logic

--- app/containers/Main/action.js
+++ app/containers/Main/action.js
@@ -30,3 +30,71 @@ export function submitForm(selectedParams) {
     console.log('dispatch', dispatch);
   }
 }
+
+
+// Sort Red keyWord
+/*
+getKeyTagsMatchedDiArr(selectors) {
+
+  let keyNameArr = []          // 被选中标红tags组
+  let keyMatchedDiArr = []     // 比对后命中疾病组
+
+  for (let i in this.currRadio) {
+    if (this.currRadio[i].match(/key/g)) keyNameArr.push(this.currRadio[i])
+  }
+
+  for (let i = 0, len = selectors.length; i < len; i++) {
+    if (selectors[i].name.match(/key/g)) keyNameArr.push(selectors[i].name)
+  }
+  console.log('标红 keyNames', keyNameArr);
+
+  for (let d in Diseases) {
+
+    let object = {}
+    object.keyMatchedNo = 0
+
+    for (let i = 0, len = keyNameArr.length; i < len; i++) {
+
+      for (let b in Diseases[d].bodyParts) {
+        if (keyNameArr[i] === b && Diseases[d].bodyParts[b]) object.keyMatchedNo++
+      }
+
+      for (let p in Diseases[d].progress) {
+        if (keyNameArr[i] === p && Diseases[d].progress[p]) object.keyMatchedNo++
+      }
+
+      for (let c in Diseases[d].checkbox) {
+        if (keyNameArr[i] === c && Diseases[d].checkbox[c]) object.keyMatchedNo++
+      }
+
+    }
+
+    object.name = d
+    object.cname = Diseases[d].cname
+    object.detail = Diseases[d] // origin Disease Detail Data
+    keyMatchedDiArr.push(object)
+
+  }
+
+  return this.getDiArrSortByFilterName(keyMatchedDiArr, 'keyMatchedNo')
+}
+*/
+/*
+// 3rd Sort prop probability
+getProbabilityMatchedDiArr(diArr) {
+  let diseaseArr = diArr
+
+  for (let d in diseaseArr) {
+    diseaseArr[d].probability = diseaseArr[d].detail['probability']
+    // 加权：base All tags
+    diseaseArr[d].probabilityWeight = (diseaseArr[d].detail['probability'] / 10 + diseaseArr[d].allTagsWeght).toFixed(5)
+
+  }
+  return this.getDiArrSortByFilterName(diseaseArr, 'probabilityWeight')
+}
+
+// 4th 重复值按照 英文排列
+getNameMatchedDiArr(diArr) {
+  return diArr
+}
+*/
--- app/containers/Main/index.js
+++ app/containers/Main/index.js
@@ -25,7 +25,6 @@ class Main extends Component {
       diArr: []
     }
 
-
     this.currRadio = {
       sex      : 'M',
       age      : '',
@@ -44,107 +43,62 @@ class Main extends Component {
   }
 
   changeSexHandler(e) {
-
     this.currRadio.sex = e.target.value
-    /*
-    let newRadio = Object.assign({}, this.state.currRadio, {
-      sex: e.target.value
-    })
-    console.log('changeSexHandler', newRadio);
-    this.setState({
-      currRadio: newRadio
-    })
-    */
   }
 
   changeAgeHandler(e) {
-
     this.currRadio.age = e.target.value
-    /*
-    let newRadio = Object.assign({}, this.state.currRadio, {
-      age: e.target.value
-    })
-    console.log('changeAgeHandler', newRadio);
-    this.setState({
-      currRadio: newRadio
-    })
-    */
   }
 
   changeBodyHandler(e) {
-
     this.currRadio.body = e.target.value
-    /*
-    console.log(e.target.value);
-    let newRadio = Object.assign({}, this.state.currRadio, {
-      body: e.target.value
-    })
-    console.log('changeBodyHandler', newRadio);
-    this.setState({
-      currRadio: newRadio
-    })
-    */
-
-    console.log('dd', this.currRadio)
   }
 
   changeContinuedHandler(e) {
-
     this.currRadio.continued = e.target.value
-    /*
-    let newRadio = Object.assign({}, this.state.currRadio, {
-      continued: e.target.value
-    })
-    console.log('changeContinuedHandler', newRadio);
-    this.setState({
-      currRadio: newRadio
-    })
-    */
   }
 
   changeProgressHandler(e) {
     this.currRadio.progress = e.target.value
-    /*
-    let newRadio = Object.assign({}, this.state.currRadio, {
-      progress: e.target.value
-    })
-    console.log('changeContinuedHandler', newRadio);
-    this.setState({
-      currRadio: newRadio
-    })
-    */
   }
-
+  /**
+   * step 1 先汇总所有选择的tag与红tag
+   * step 2
+   * @return {[type]} [description]
+   */
   getCheckboxValues() {
 
-    const rootDom = this.refs.checkbox
-    let selectors = rootDom.querySelectorAll('div input[type="checkbox"]:checked') || []
-    let sortedDiArr = [];
+    let selectors = this.refs.checkbox.querySelectorAll('div input[type="checkbox"]:checked') || []
+    let sortedDiArr = this.getTagsMatchedDiArr(selectors)
 
-    sortedDiArr = this.getKeyTagsMatchedDiArr(selectors)
+console.log('finished sortedDiArr::::', sortedDiArr);
 
-    sortedDiArr = this.getAllTagsMatchedDiArr(selectors, sortedDiArr)
+    let baseRankArr = this.getDiArrFilterByWeekTags(this.getDiArrByCountKeys(sortedDiArr))
+console.log('baseRankArr::::::', baseRankArr);
 
-    this.getDiArrByCountKeys(sortedDiArr);
-
-
+    sortedDiArr = this.concatRankDiArr(baseRankArr)
     /*
     sortedDiArr = this.getProbabilityMatchedDiArr(sortedDiArr)
-
     sortedDiArr = this.getNameMatchedDiArr(sortedDiArr)
     */
-    console.log('finished sortedDiArr', sortedDiArr);
+console.log('sortedDiArr:::', sortedDiArr);
 
     return sortedDiArr
   }
 
+  concatRankDiArr(rankDiArr) {
+    let baseDiArr = [];
+    rankDiArr.length > 0 && rankDiArr.map((subArr, idx) => {
+      console.log(subArr);
+      baseDiArr = baseDiArr.concat(subArr)
+      console.log('baseDiArr' + idx, baseDiArr);
+    })
 
+    return baseDiArr;
+  }
 
   /**
    * 通过特定标识排序
-   * @param  {[type]} diArr      [description]
-   * @param  {[type]} filterProp [description]
-   * @return {[type]}            [description]
    */
   getDiArrSortByFilterName(diArr, filterProp) {
     return diArr.length > 0  && diArr.sort(function(a, b) {
@@ -176,39 +130,56 @@ class Main extends Component {
     for (let o in baseObj) {
       rankDiArr.push(baseObj[o])
     }
-
-    console.log('baseObj:::', baseObj);
-    console.log('rankDiArr', rankDiArr);
-
+    // 直接排序
+    return this.getSubDiArrSortByAllTagsNo(rankDiArr)
   }
 
-  getSubDiArrByCountKeys(diArr) {
-    
+  /**
+   * step2 result: 根据所有tag数排序子数组
+   */
+  getSubDiArrSortByAllTagsNo(rankDiArr) {
+    let newRankArr = []
+    rankDiArr.length > 0 && rankDiArr.map((arr, idx) => {
+      newRankArr.push(this.getDiArrSortByFilterName(arr, 'allTagsMatchedNo'))
+    })
+    return newRankArr
   }
 
+  // Sort all tags
+  getTagsMatchedDiArr(selectors) {
 
-
-
-  // 1st Sort Red keyWord
-  getKeyTagsMatchedDiArr(selectors) {
-
-    let keyNameArr = []          // 被选中标红tags组
-    let keyMatchedDiArr = []     // 比对后命中疾病组
+    let keyNameArr = []              // 被选中标红tags组
+    let allTagsNameArr = []          // 被选中所有tags组
+    let countedArr = []
 
     for (let i in this.currRadio) {
+      // red tags
       if (this.currRadio[i].match(/key/g)) keyNameArr.push(this.currRadio[i])
+      // all tags
+      if (!i.match(/sex|continued/g)) {
+        allTagsNameArr.push({
+          [i]: this.currRadio[i]
+        })
+      }
     }
 
     for (let i = 0, len = selectors.length; i < len; i++) {
+      // red tags
       if (selectors[i].name.match(/key/g)) keyNameArr.push(selectors[i].name)
+      // black + red + green
+      allTagsNameArr.push(selectors[i].name)
     }
-    console.log('标红 keyNames', keyNameArr);
+
+    console.log('key tags 选中的红色标签::::::', keyNameArr);
+    console.log('all tags 选中的所有标签::::::', allTagsNameArr);
 
     for (let d in Diseases) {
 
       let object = {}
-      object.keyMatchedNo = 0
-
+      object.keyMatchedNo = 0      // red tags
+      object.allTagsMatchedNo = 0  // all tags
+      object.weekTagsMatchedNo = 0 // green tags
+      // 计算红tag个数
       for (let i = 0, len = keyNameArr.length; i < len; i++) {
 
         for (let b in Diseases[d].bodyParts) {
@@ -228,95 +199,66 @@ class Main extends Component {
       object.name = d
       object.cname = Diseases[d].cname
       object.detail = Diseases[d] // origin Disease Detail Data
-      keyMatchedDiArr.push(object)
+      // ------------------------------------------------------------------------
 
-    }
-
-    return this.getDiArrSortByFilterName(keyMatchedDiArr, 'keyMatchedNo')
-  }
-
-  // 2nd Sort all tags
-  getAllTagsMatchedDiArr(selectors, diArr) {
-    let allTagsNameArr = []          // 被选中所有tags组
-    let allTagsMatchedDiArr = []     // 比对后命中疾病组
-    let diseaseArr = diArr
-
-    for (let i in this.currRadio) {
-      if (!i.match(/sex|continued/g)) {
-        allTagsNameArr.push({
-          [i]: this.currRadio[i]
-        })
-      }
-    }
-
-    for (let i = 0, len = selectors.length; i < len; i++) {
-      allTagsNameArr.push(selectors[i].name)
-    }
-    console.log('所有选中标签::::::', allTagsNameArr);
-
-    for (let d in diseaseArr) {
-
-      diseaseArr[d].allTagsMatchedNo = 0
-
+      // 计算所有tag个数
       for (let i = 0, len = allTagsNameArr.length; i < len; i++) {
 
         if (typeof allTagsNameArr[i] === 'object') {
           // ageLimit
           if (allTagsNameArr[i]['age']) {
-            +allTagsNameArr[i]['age'] >= diseaseArr[d].detail.ageLimit[0] && +allTagsNameArr[i]['age'] <= diseaseArr[d].detail.ageLimit[1] &&
-              diseaseArr[d].allTagsMatchedNo++
+            +allTagsNameArr[i]['age'] >= object.detail.ageLimit[0] && +allTagsNameArr[i]['age'] <= object.detail.ageLimit[1] &&
+              object.allTagsMatchedNo++
           }
 
           if (allTagsNameArr[i]['body']) {
-            for (let b in diseaseArr[d].detail.bodyParts) {
-              if (b === allTagsNameArr[i]['body'] && diseaseArr[d].detail.bodyParts[b]) {
-                diseaseArr[d].allTagsMatchedNo++
+            for (let b in object.detail.bodyParts) {
+              // console.log(b);
+              if (b === allTagsNameArr[i]['body'] && object.detail.bodyParts[b]) {
+                object.allTagsMatchedNo++
               }
             }
           }
 
           if (allTagsNameArr[i]['progress']) {
-            for (let p in diseaseArr[d].detail.progress) {
-              if (p === allTagsNameArr[i]['progress'] && diseaseArr[d].detail.progress[p]) {
-                diseaseArr[d].allTagsMatchedNo++
+            for (let p in object.detail.progress) {
+              // console.log(p);
+              if (p === allTagsNameArr[i]['progress'] && object.detail.progress[p]) {
+                object.allTagsMatchedNo++
               }
             }
           }
 
         } else {
 
-          for (let c in diseaseArr[d].detail.checkbox) {
-            if (allTagsNameArr[i] === c && diseaseArr[d].detail.checkbox[c]) diseaseArr[d].allTagsMatchedNo++
+          for (let c in object.detail.checkbox) {
+
+            if (allTagsNameArr[i] === c && object.detail.checkbox[c]) object.allTagsMatchedNo++
+            if (allTagsNameArr[i] === c && object.detail.checkbox[c] && !!c.match(/week/g)) object.weekTagsMatchedNo++
           }
 
         }
 
       }
-      // 加权值 = (key * 0.1 + 1) + allTag[重]
-      // diseaseArr[d].allTagsWeght = 1 + diseaseArr[d].keyMatchedNo * 0.1 + diseaseArr[d].allTagsMatchedNo
+
+      countedArr.push(object)
 
     }
 
-    return this.getDiArrSortByFilterName(diseaseArr, 'allTagsWeght')
+    return this.getDiArrSortByFilterName(countedArr, 'keyMatchedNo');
 
   }
 
-  // 3rd Sort prop probability
-  getProbabilityMatchedDiArr(diArr) {
-    let diseaseArr = diArr
-
-    for (let d in diseaseArr) {
-      diseaseArr[d].probability = diseaseArr[d].detail['probability']
-      // 加权：base All tags
-      diseaseArr[d].probabilityWeight = (diseaseArr[d].detail['probability'] / 10 + diseaseArr[d].allTagsWeght).toFixed(5)
-
-    }
-    return this.getDiArrSortByFilterName(diseaseArr, 'probabilityWeight')
-  }
-
-  // 4th 重复值按照 英文排列
-  getNameMatchedDiArr(diArr) {
-    return diArr
+  getDiArrFilterByWeekTags(rankDiArr) {
+    rankDiArr.length > 0 && rankDiArr.map((subArr, index) => {
+      subArr.map((o, idx) => {
+        if (o.weekTagsMatchedNo > 0 && index > 0) {
+          rankDiArr[index - 1].push(o) // 有week则提升一个分组
+          subArr.splice(idx, 1)        // current数组删除
+        }
+      })
+    })
+    return rankDiArr
   }
 
   // 提交表单
@@ -324,8 +266,6 @@ class Main extends Component {
     const { Actions } = this.props
 
     const diArr = this.getCheckboxValues()
-
-    console.log('diArr:::', diArr)
 
     this.setState({
       diArr: diArr
@@ -337,30 +277,30 @@ class Main extends Component {
 
     const { Actions } = this.props
     const { diArr } = this.state
-    console.log(diArr);
-    return (
-      <div>
 
-        <div>
-          <div>性别：</div>
+    return (
+      <div style={{margin: '20px 20px'}}>
+
+        <div style={{marginBottom: '10px'}}>
+          <div style={{fontSize: '16px'}}>性别：</div>
           <div>
             男 <input name="sex" type="radio" value="M" defaultChecked onClick={this.changeSexHandler}/>
             女 <input name="sex" type="radio" value="F" onClick={this.changeSexHandler}/>
           </div>
         </div>
-        <div>
+        <div style={{marginBottom: '10px'}}>
           年龄：<input type="form" onChange={this.changeAgeHandler}/>
         </div>
-        <div>
-          发病部位：
+        <div style={{marginBottom: '10px'}}>
+          <div style={{fontSize: '16px'}}>发病部位：</div>
           <div>
             左侧 <input name="bodyParts" type="radio" defaultValue="left" defaultChecked onClick={this.changeBodyHandler}/>
             右侧 <input name="bodyParts" type="radio" defaultValue="right"  onClick={this.changeBodyHandler}/>
             <span style={{color: 'red'}}>双侧</span><input name="bodyParts" type="radio" defaultValue="key_both" onClick={this.changeBodyHandler}/>
           </div>
         </div>
-        <div>
-          面瘫持续时间：
+        <div style={{marginBottom: '10px'}}>
+          <div style={{fontSize: '16px'}}>面瘫持续时间：</div>
           <div>
             1周内 <input name="continued" type="radio" value="0" defaultChecked onClick={this.changeContinuedHandler} />
             1周~1月 <input name="continued" type="radio" value="1" onClick={this.changeContinuedHandler} />
@@ -368,16 +308,16 @@ class Main extends Component {
             6月以上 <input name="continued" type="radio" value="3" onClick={this.changeContinuedHandler} />
           </div>
         </div>
-        <div>
-          进展：
+        <div style={{marginBottom: '10px'}}>
+          <div style={{fontSize: '16px'}}>进展：</div>
           <div>
             <span style={{color: 'red'}}>1天内</span> <input name="progress" type="radio" value="key_day" defaultChecked onClick={this.changeProgressHandler}/>
             1天~1周 <input name="progress" type="radio" value="week" onClick={this.changeProgressHandler} />
             1周以上 <input name="progress" type="radio" value="exceedWeek" onClick={this.changeProgressHandler} />
           </div>
         </div>
-        <div>
-          有无：
+        <div style={{marginBottom: '10px'}}>
+          <div style={{fontSize: '16px'}}>有无：</div>
           <div ref="checkbox">
             <div>
               <input name="fever" type="checkbox"/>发烧
@@ -394,9 +334,9 @@ class Main extends Component {
               <input name="key_eye" type="checkbox" /><span style={{color: 'red'}}>闭眼困难</span>
             </div>
             <div>
-              <input name="weekTeeth" type="checkbox" />一周内曾拔牙
-              <input name="weekFace" type="checkbox" />一周内曾接受面部麻醉
-              <input name="weekVaccine" type="checkbox" />一周内曾打疫苗
+              <input name="weekTeeth" type="checkbox" /><span style={{color: 'green'}}>一周内曾拔牙</span>
+              <input name="weekFace" type="checkbox" /><span style={{color: 'green'}}>一周内曾接受面部麻醉</span>
+              <input name="weekVaccine" type="checkbox" /><span style={{color: 'green'}}>一周内曾打疫苗</span>
             </div>
             <div>
               <input name="tired" type="checkbox" />易疲劳
@@ -416,7 +356,7 @@ class Main extends Component {
           </div>
         </div>
 
-        <div>
+        <div style={{marginBottom: '10px'}}>
           <div style={{
             border: '1px solid #bbb',
             width: '150px',
@@ -427,7 +367,7 @@ class Main extends Component {
           }} onClick={this.submitFormHandler}>确认提交</div>
         </div>
         <div>
-          <h3>选择的病可能是：</h3>
+          <div style={{fontSize: '16px'}}>选择的病可能是：</div>
           {
             diArr.map((di, idx) => {
               return (
